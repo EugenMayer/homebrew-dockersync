04b57d4c5d394affc099768ea9fc7682b2f8a1f2
em <eugen.mayer@kontextwork.de>
2017-04-24 21:33:03 +0200

    polishing

--- Formula/unox.rb
+++ Formula/unox.rb
@@ -1,24 +1,23 @@
 class Unox < Formula
   # see http://docs.brew.sh/Python-for-Formula-Authors.html
-  desc "A layer for unison-fsmonitor arround either macfsevents or watchdog for usage with unison"
-  homepage "https://github.com/hnsl/unox"
-  url "https://codeload.github.com/hnsl/unox/tar.gz/0.2.0"
+  desc 'A layer for unison-fsmonitor arround either macfsevents or watchdog for usage with unison'
+  homepage 'https://github.com/hnsl/unox'
+  url 'https://codeload.github.com/hnsl/unox/tar.gz/0.2.0'
   head 'https://github.com/hnsl/unox.git', :using => :git, :revision => '0.2'
   sha256 '163668398356619d0b422f7e067836754d6eae6bf63058bac3f4c7c70182e837'
   revision 1
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
   depends_on :python3 => :optional
 
-  resource "watchdog" do
-    url "https://pypi.python.org/packages/54/7d/c7c0ad1e32b9f132075967fc353a244eb2b375a3d2f5b0ce612fd96e107e/watchdog-0.8.3.tar.gz"
-    sha256 "7e65882adb7746039b6f3876ee174952f8eaaa34491ba34333ddf1fe35de4162"
+  resource 'watchdog' do
+    url 'https://pypi.python.org/packages/54/7d/c7c0ad1e32b9f132075967fc353a244eb2b375a3d2f5b0ce612fd96e107e/watchdog-0.8.3.tar.gz'
+    sha256 '7e65882adb7746039b6f3876ee174952f8eaaa34491ba34333ddf1fe35de4162'
   end
 
-  resource "pathtools" do
-    url "https://pypi.python.org/packages/e7/7f/470d6fcdf23f9f3518f6b0b76be9df16dcc8630ad409947f8be2eb0ed13a/pathtools-0.1.2.tar.gz"
-    sha256 "7c35c5421a39bb82e58018febd90e3b6e5db34c5443aaaf742b3f33d4655f1c0"
+  resource 'pathtools' do
+    url 'https://pypi.python.org/packages/e7/7f/470d6fcdf23f9f3518f6b0b76be9df16dcc8630ad409947f8be2eb0ed13a/pathtools-0.1.2.tar.gz'
+    sha256 '7c35c5421a39bb82e58018febd90e3b6e5db34c5443aaaf742b3f33d4655f1c0'
   end
-
 
   include Language::Python::Virtualenv
 
